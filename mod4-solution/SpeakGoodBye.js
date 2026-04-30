d6a036cb101fdd10ecce220976ddc8640c4c02c2
Felipe <f.dachshund@gmail.com>
2026-04-30 11:23:46 -0300

    [4] Create byeSpeaker (step 7)

--- mod4-solution/SpeakGoodBye.js
+++ mod4-solution/SpeakGoodBye.js
@@ -1,14 +1,14 @@
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
-// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+// [DONE] STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 
 (function (window) {
-  // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+  // [DONE] STEP 7: Create an object, called 'byeSpeaker' to which you will attach
   // the "speak" method and which you will expose to the global context
   // See Lecture 52, part 1
-  // var byeSpeaker =
+  var byeSpeaker = {};
 
   // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
   var speakWord = "Good Bye";
